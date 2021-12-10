e85b6ef9a414f717446d3bff0ee43695dc048195
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-10 12:17:56 +0100

    dev: added interceptor

--- projects/jlg/src/public-api.ts
+++ projects/jlg/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/jlg.component';
 export * from './lib/jlg.module';
 export * from './lib/jlg-widgets/jlg-widgets.module';
 export * from './lib/jlg-widgets/autofocus.directive';
+export * from './lib/interceptors/credentials.interceptor';
