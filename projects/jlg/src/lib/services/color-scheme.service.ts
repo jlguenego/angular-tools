6e724782a7269f0e48d81fb2a089308430aa494e
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-16 15:45:33 +0100

    dev: color scheme and theme

--- projects/jlg/src/lib/services/color-scheme.service.ts
+++ projects/jlg/src/lib/services/color-scheme.service.ts
@@ -10,7 +10,9 @@ export class ColorSchemeService {
   colorScheme$ = new BehaviorSubject<ColorScheme>('light');
 
   constructor() {
+    this.initUserPreferences();
     this.syncWithUserPreferences();
+    this.setFavicon(this.colorScheme$.value);
     this.colorScheme$.subscribe((newColorScheme) => {
       ['dark', 'light'].forEach((colorScheme) => {
         document.body.classList.remove(colorScheme);
@@ -28,9 +30,7 @@ export class ColorSchemeService {
     this.colorScheme$.next('light');
   }
 
-  syncWithUserPreferences() {
-    this.initUserPreferences();
-
+  private syncWithUserPreferences() {
     // also watch for user pref changes.
     if (!window.matchMedia) {
       return;
@@ -40,10 +40,11 @@ export class ColorSchemeService {
       .addEventListener('change', (e) => {
         const colorScheme = e.matches ? 'dark' : 'light';
         this.colorScheme$.next(colorScheme);
+        this.setFavicon(colorScheme);
       });
   }
 
-  initUserPreferences() {
+  private initUserPreferences() {
     const str = localStorage.getItem('color-scheme') as ColorScheme;
     if (!['dark', 'light'].includes(str)) {
       localStorage.removeItem('color-scheme');
@@ -56,5 +57,17 @@ export class ColorSchemeService {
       return;
     }
     this.colorScheme$.next(str);
+  }
+
+  setFavicon(newColorScheme: ColorScheme) {
+    let link: HTMLLinkElement | null =
+      document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      link.type = 'image/svg';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    link.href = `assets/favicon-${newColorScheme}.svg`;
   }
 }
