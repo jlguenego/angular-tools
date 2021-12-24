677e4c630df2f90c6087d3c55369ac9d9ed7bcd3
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-12-24 12:16:38 +0100

    dev: delete jlg service

--- projects/jlg/src/public-api.ts
+++ projects/jlg/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of jlg
  */
 
-export * from './lib/jlg.service';
 export * from './lib/jlg.component';
 export * from './lib/jlg.module';
 export * from './lib/jlg-widgets/jlg-widgets.module';
