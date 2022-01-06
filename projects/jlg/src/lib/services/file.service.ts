1f3fe40ad3decfa969e6fc69ce35ee884ab478a9
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-06 12:09:30 +0100

    dev: added image compression

--- projects/jlg/src/lib/services/file.service.ts
+++ projects/jlg/src/lib/services/file.service.ts
@@ -1,6 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import imageCompression from 'browser-image-compression';
+import { lastValueFrom } from 'rxjs';
+
+export interface FileServiceOptions {
+  compress?: {
+    maxSizeMB?: number;
+    maxWidthOrHeight?: number;
+    useWebWorker?: boolean;
+    maxIteration?: number;
+    exifOrientation?: number;
+    onProgress?: (progress: number) => void;
+    fileType?: string;
+    initialQuality?: number;
+  };
+}
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +22,26 @@ import { Injectable } from '@angular/core';
 export class FileService {
   constructor(private http: HttpClient) {}
 
-  add(image: File): Observable<{ url: string }> {
+  async add(image: File, options: FileServiceOptions = {}): Promise<void> {
+    let compressedImage = image;
+    if (options.compress) {
+      // compress before sending
+      console.log('about to compress');
+      try {
+        const file = await imageCompression(image, options.compress);
+        compressedImage = new File([file], image.name, {
+          type: image.type,
+          lastModified: image.lastModified,
+        });
+        console.log('compressedImage: ', compressedImage);
+      } catch (err) {
+        console.log('err: ', err);
+        throw err;
+      }
+    }
     const formData = new FormData();
-    formData.append('file', image);
-    return this.http.post<{ url: string }>('/api/upload', formData);
+    formData.append('file', compressedImage);
+    await lastValueFrom(this.http.post<void>('/api/upload', formData));
   }
 
   getUrl(name: string): string {
