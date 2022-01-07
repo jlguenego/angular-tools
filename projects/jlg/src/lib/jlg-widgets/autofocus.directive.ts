4f0716543108d5e1ac583de913a4071c62fb69ad
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-01-07 13:59:16 +0100

    dev: added isSmartphone

--- projects/jlg/src/lib/jlg-widgets/autofocus.directive.ts
+++ projects/jlg/src/lib/jlg-widgets/autofocus.directive.ts
@@ -6,6 +6,12 @@ import {
   OnInit,
 } from '@angular/core';
 
+const isSmartphone = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+};
+
 @Directive({
   selector: '[jlgAutofocus]',
 })
@@ -15,6 +21,9 @@ export class AutofocusDirective implements AfterContentInit {
     console.log('jlg autofocus');
   }
   ngAfterContentInit(): void {
+    if (isSmartphone()) {
+      return;
+    }
     if (this.jlgAutofocus === 'select') {
       this.elt.nativeElement.select();
       return;
